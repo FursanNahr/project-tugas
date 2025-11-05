73b5cd77f9c9e4b25d2831ab083ce210f649b958
FursanNahr <fursan10nahr@gmail.com>
2025-11-05 20:48:38 +0700

    feat: kalkulator penjumlahan

--- studikasus.py
+++ studikasus.py
@@ -6,3 +6,9 @@ fahrenheit = (celsius * 9/5) + 32
 
 print("sebelum diubah ke fahrenheit: ", celsius)
 print("setelah diubah ke farenheit: ", fahrenheit)
+
+input_1 = int(input("Masukkan bilangan 1: "))
+input_2 = int(input("Masukkan bilangan 2: "))
+hasil = input_1 + input_2
+
+print("Hasil pertambahannya adalah: ", hasil)
